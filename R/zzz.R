87c2d445856c4d20ff83d846b42ccde8ab32a574
Mathias Leroy <mathiasleroy@users.noreply.github.com>
2026-03-31 09:28:23 +0200

    add comment for source

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 # ensure S7 methods are registered before S3 dispatch happens
+# cf. https://rconsortium.github.io/S7/articles/packages.html
 .onLoad <- function(libname, pkgname) {
   S7::methods_register()
 }
